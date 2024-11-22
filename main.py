813deb52814f431be1c9595a5e4f6a8ab63a1f2b
Jaelucard <lowjarrod@gmail.com>
2024-11-22 13:09:29 +0800

    Enigma Game

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ import tkinter as tk
 import random
 
 class EnigmaGame:
-    def algorithm:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Enigma Puzzle Game")
+        self.level = 1
+        self.score = 0
+        self.hints_used = 0
+        self.remaining_attempts = 3
+        self.current_step = 1
+        self.cipher = {}
+        self.secret_message = ""
+        self.encoded_message = ""
+        self.scrambled_word = ""
+        self.math_problem = ""
+        self.math_solution = 0
+        
     
