e45bca53a976e143f788c04b9611afeccc400153
Natalie Lim <nutellalimjy@gmail.com>
2024-11-28 02:41:37 +0800

    full encrypt and decrypt

--- validation.py
+++ validation.py
@@ -7,10 +7,10 @@ class Validation:
             try:
                 value = int(input(prompt).replace(" ", ""))  #check if input is an integer
                 if variable == 'm':
-                    if gcd(value, 26) == 1:  #check if the value is coprime with 26
+                    if gcd(value, 21) == 1:  #check if the value is coprime with 21 (21 letters left after removing vowels)
                         return value
                     else:
-                        print("Invalid value. Please choose a value (or its negative) that is coprime with 26 (i.e., its greatest common divisor with 26 is 1).\n")
+                        print("Invalid value. Please choose a value (or its negative) that is coprime with 21 (i.e., the only common factor between 21 and m is 1).\n")
                 else:
                     return value
             except ValueError:  # If conversion fails, prompt again
