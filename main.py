ec652e44182d78142aca35d765c78c19197947f6
Jaelucard <lowjarrod@gmail.com>
2024-12-09 18:44:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -194,21 +194,3 @@ class RunProgramme:
 
 run = RunProgramme()
 run.start()
-
-class EnigmaGame:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Enigma Puzzle Game")
-        self.level = 1
-        self.score = 0
-        self.hints_used = 0
-        self.remaining_attempts = 3
-        self.current_step = 1
-        self.cipher = {}
-        self.secret_message = ""
-        self.encoded_message = ""
-        self.scrambled_word = ""
-        self.math_problem = ""
-        self.math_solution = 0
-        
-    
